954129dec6ba05c9230b433aef7c55eb7dfa5785
Rogerio Fonseca <rogeriof86@gmail.com>
2026-09-06 01:00:44 -0300

    feat: aponta front-end para o Supabase unificado portal

--- assets/supabase-client.js
+++ assets/supabase-client.js
@@ -1,8 +1,8 @@
 (function () {
   'use strict';
 
-  const SUPABASE_URL = "https://qqefegpievdlaprwzktx.supabase.co";
-  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxZWZlZ3BpZXZkbGFwcnd6a3R4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0ODQxNzAsImV4cCI6MjA5ODA2MDE3MH0.1h_585aBtTnXtA0FLtbNvhkYpVokgfHbUpKcAIBImKY";
+  const SUPABASE_URL = "https://lucpxoynpvogkvzepagi.supabase.co";
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx1Y3B4b3lucHZvZ2t2emVwYWdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MDYxNzQsImV4cCI6MjA5OTE4MjE3NH0.r0XGYX1KqAXQA4g9uoUAFLFTEaWUEXobWqyKVe0_SnE";
 
   window.SirFisherSupabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 })();
